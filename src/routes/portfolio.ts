3622f1b41accef3f4985441880b736b07ce8014b
Romil <khandryga.romil@gmail.com>
2025-03-26 11:12:43 +0300

    feat: add single search

--- src/routes/portfolio.ts
+++ src/routes/portfolio.ts
@@ -1,3 +1,6 @@
+
+//@ts-nocheck
+
 import { Router, Request, Response } from 'express'
 import portfolio from '../data/portfolio'
 
@@ -5,6 +8,16 @@ const router = Router()
 
 router.get('/', (_: Request, res: Response) => {
   res.json(portfolio)
+})
+router.get('/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+  const item = portfolio.find((item) => item.id === id)
+
+  if (!item) {
+    return res.status(404).json({ error: 'Item not found' })
+  }
+
+  res.json(item)
 })
 
 export default router
--- src/routes/services.ts
+++ src/routes/services.ts
@@ -1,3 +1,6 @@
+
+//@ts-nocheck
+
 import { Router, Request, Response } from 'express'
 import services from '../data/services'
 
@@ -5,6 +8,18 @@ const router = Router()
 
 router.get('/', (_: Request, res: Response) => {
   res.json(services)
+})
+
+
+router.get('/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+  const item = services.find((item) => item.id === id)
+
+  if (!item) {
+    return res.status(404).json({ error: 'Item not found' })
+  }
+
+  res.json(item)
 })
 
 export default router
